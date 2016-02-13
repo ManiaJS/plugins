c7d5e1985eed22d981f434ed426dfd94efb48498
Tom Valk <tomvalk@gmail.com>
2016-02-13 11:05:20 +0100

    Remove the null assignment of app.

--- plugin.js
+++ plugin.js
@@ -16,9 +16,6 @@ export default class {
     // Process the plugin information. (Verify should be done by the core).
     this.plugin = plugin;
 
-    // App will be replaced with the app context of the core.
-    this.app = null;
-
     // Make our own plugin main interface an event emitter.
     // This can be used to communicate and subscribe to others plugins events.
     EventEmitter.call(this);
