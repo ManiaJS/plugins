4e84ed919d549167d3bafab448958bee6d5611df
Tom Valk <tomvalk@gmail.com>
2016-02-13 13:19:14 +0100

    Update plugin structure, use classes.

--- plugin.js
+++ plugin.js
@@ -4,20 +4,16 @@ import { EventEmitter } from 'events';
 
 /**
  * Plugin Interface
- * @class
+ * @class Plugin
  */
 export default class {
 
   /**
    * Construct the plugin.
-   * @param {object} plugin
    */
-  constructor(plugin) {
-    // Process the plugin information. (Verify should be done by the core).
-    this.plugin = plugin;
-
+  constructor() {
     // Make our own plugin main interface an event emitter.
     // This can be used to communicate and subscribe to others plugins events.
-    EventEmitter.call(this.plugin);
+    EventEmitter.call(this);
   }
 }
