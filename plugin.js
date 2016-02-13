8b690a715006997b5c6bc550438248c8f9a2b8cb
Tom Valk <tomvalk@gmail.com>
2016-02-13 10:55:24 +0100

    Adding abstract plugin class basics.

--- plugin.js
+++ plugin.js
@@ -1,10 +1,26 @@
 'use strict';
 
+import { EventEmitter } from 'events';
+
 /**
  * Plugin Interface
+ * @class
  */
 export default class {
-  constructor() {
 
+  /**
+   * Construct the plugin.
+   * @param {object} plugin
+   */
+  constructor(plugin) {
+    // Process the plugin information. (Verify should be done by the core).
+    this.plugin = plugin;
+
+    // App will be replaced with the app context of the core.
+    this.app = null;
+
+    // Make our own plugin main interface an event emitter.
+    // This can be used to communicate and subscribe to others plugins events.
+    EventEmitter.call(this);
   }
 }
