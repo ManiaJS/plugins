66a69a20acab5504c57da6e28438d1ca4d227f4b
Tom Valk <tomvalk@gmail.com>
2016-02-13 13:24:10 +0100

    Adding in empty fields.

--- plugin.js
+++ plugin.js
@@ -12,6 +12,13 @@ export default class {
    * Construct the plugin.
    */
   constructor() {
+    // Will be replaced by core
+    this.app = null;
+    this.plugins = null;
+    this.server = null;
+    this.players = null;
+    this.maps = null;
+
     // Make our own plugin main interface an event emitter.
     // This can be used to communicate and subscribe to others plugins events.
     EventEmitter.call(this);
