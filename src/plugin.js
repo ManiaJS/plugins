15d0ffcefafa942ab88fd3d8420607a7bbb9bf8b
Max Klaversma <maxklaversma@gmail.com>
2016-02-20 15:16:23 +0100

    Karma widget added.

--- src/plugin.js
+++ src/plugin.js
@@ -25,6 +25,89 @@ module.exports.default = class extends Plugin {
     this.votes = null;
     this.plusVotes = 0;
     this.minVotes = 0;
+
+    this.widgetEnabled = true;
+    this.widgetEntries = 16;
+    this.widgetTopCount = 3;
+    this.widgetWidth = 15.5;
+    this.widgetHeight = 11;
+    this.widgetX = 49.2;
+    this.widgetY = 39.2;
+
+    this.sideSettings = {
+      left: {
+        icon: {
+          x: 0.6,
+          y: 0
+        },
+        title: {
+          x: 3.2,
+          y: -0.65,
+          halign: 'left'
+        },
+        image_open: {
+          x: -0.3,
+          image: 'http://static.undef.name/ingame/records-eyepiece/edge-open-ld-dark.png'
+        }
+      },
+      right: {
+        icon: {
+          x: 12.5,
+          y: 0
+        },
+        title: {
+          x: 12.4,
+          y: -0.65,
+          halign: 'right'
+        },
+        image_open: {
+          x: 12.2,
+          image: 'http://static.undef.name/ingame/records-eyepiece/edge-open-rd-dark.png'
+        }
+      }
+    };
+
+    this.widgetSettings = {
+      manialinkid: 'Karma',
+      actionid: 'OpenKarma',
+      title: 'Map Karma',
+
+      positiveaction: 'votePositive',
+      negativeaction: 'voteNegative',
+
+      width: this.widgetWidth,
+      height: this.widgetHeight,
+      column_height: (this.widgetHeight - 3.1),
+      widget_x: this.widgetX,
+      widget_y: this.widgetY,
+      background_width: (this.widgetWidth - 0.2),
+      background_height: (this.widgetHeight - 0.2),
+      border_width: (this.widgetWidth + 0.4),
+      border_height: (this.widgetHeight + 0.6),
+
+      background_color: '3342',
+      background_focus: '09F6',
+      background_style: 'Bgs1',
+      background_substyle: 'BgTitleGlow',
+      border_style: 'Bgs1',
+      border_substyle: 'BgTitleShadow',
+
+      image_open_x: (this.widgetX < 0) ? this.sideSettings.right.image_open.x + (this.widgetWidth - 15.5) : this.sideSettings.left.image_open.x,
+      image_open_y: -(this.widgetHeight - 3.18),
+      image_open: (this.widgetX < 0) ? this.sideSettings.right.image_open.image : this.sideSettings.left.image_open.image,
+
+      title_background_width: (this.widgetWidth - 0.8),
+      title_style: 'BgsPlayerCard',
+      title_substyle: 'BgRacePlayerName',
+      title_x: (this.widgetX < 0) ? this.sideSettings.right.title.x + (this.widgetWidth - 15.5) : this.sideSettings.left.title.x,
+      title_y: (this.widgetX < 0) ? this.sideSettings.right.title.y : this.sideSettings.left.title.y,
+      title_halign: (this.widgetX < 0) ? this.sideSettings.right.title.halign : this.sideSettings.left.title.halign,
+
+      icon_x: (this.widgetX < 0) ? this.sideSettings.right.icon.x + (this.widgetWidth - 15.5) : this.sideSettings.left.icon.x,
+      icon_y: (this.widgetX < 0) ? this.sideSettings.right.icon.y : this.sideSettings.left.icon.y,
+      icon_style: 'Icons128x128_1',
+      icon_substyle: 'NewTrack'
+    };
   }
 
   /**
@@ -42,6 +125,7 @@ module.exports.default = class extends Plugin {
       this.server.on('player.connect', (params) => {
         let player = this.players.list[params.login];
         this.displayVotes(player);
+        this.displayKarmaWidget(player);
       });
 
       this.server.command.on('skip', 1, (player, params) => {
@@ -54,15 +138,34 @@ module.exports.default = class extends Plugin {
         this.server.send().custom('RestartMap').exec();
       });
 
-      this.server.command.on('++', 0, (player, params) => this.votePositive(player));
-      this.server.command.on('--', 0, (player, params) => this.voteNegative(player));
+      this.server.command.on('update', 1, (player, params) => {
+        let plyr = this.players.list[player.login];
+        this.displayKarmaWidget(plyr);
+      });
+
+      // UI
+      this.karmaWidget = this.app.ui.build(this, 'karmawidget', 1);
+      this.karmaWidget.global(this.widgetSettings);
+
+      this.server.command.on('++', 0, (player, params) => this.votePositive(player, false));
+      this.server.command.on('--', 0, (player, params) => this.voteNegative(player, false));
       this.server.on('player.chat', (params) => {
         let player = this.players.list[params.login];
         if(params.text == '++') {
-          this.votePositive(player);
+          this.votePositive(player, false);
         } else if(params.text == '--') {
-          this.voteNegative(player);
+          this.voteNegative(player, false);
         }
+      });
+
+      this.karmaWidget.on('votePositive', (data) => {
+        let player = this.players.list[data.login];
+        this.votePositive(player, true);
+      });
+
+      this.karmaWidget.on('voteNegative', (data) => {
+        let player = this.players.list[data.login];
+        this.voteNegative(player, true);
       });
 
       this.loadVotes(this.maps.current).then(() => {
@@ -104,6 +207,7 @@ module.exports.default = class extends Plugin {
       Object.keys(this.players.list).forEach((login) => {
         let player = this.players.list[login];
         this.displayVotes(player);
+        this.displayKarmaWidget(player);
       });
     });
   }
@@ -115,8 +219,8 @@ module.exports.default = class extends Plugin {
    */
   displayVotes(player) {
     var chatKarma = '$ff0Current map karma: $fff' + (this.plusVotes - this.minVotes);
-    chatKarma += '$ff0 [$fff' + this.votes.length + '$ff0 votes, ++: $fff' + this.plusVotes + '$ff0 ($fff' + Math.round(((this.plusVotes/this.votes.length) * 100)) + '%$ff0)';
-    chatKarma += ' --: $fff' + this.minVotes + '$ff0 ($fff' + Math.round(((this.minVotes/this.votes.length) * 100)) + '%$ff0)]';
+    chatKarma += '$ff0 [$fff' + this.votes.length + '$ff0 votes, ++: $fff' + this.plusVotes + '$ff0 ($fff' + parseFloat(((this.plusVotes/this.votes.length) * 100)).toFixed(1) + '%$ff0)';
+    chatKarma += ' --: $fff' + this.minVotes + '$ff0 ($fff' + parseFloat(((this.minVotes/this.votes.length) * 100)).toFixed(1) + '%$ff0)]';
 
     var playerVote = this.votes.filter(function (vote) {
       return vote.PlayerId == player.id;
@@ -139,18 +243,84 @@ module.exports.default = class extends Plugin {
   }
 
   /**
+   * Displays the karma widget to the player.
+   *
+   * @param player
+   */
+  displayKarmaWidget(player) {
+    var votebars = [];
+    if(this.votes.length > 0) {
+      for(var i = 0; i < Math.round((this.plusVotes / this.votes.length) * 10); i++) {
+        votebars.push({
+          color: 'green',
+          x: (1.5 + (i * 1.2))
+        });
+      }
+      for(var i = Math.round((this.plusVotes / this.votes.length) * 10); i < 10; i++) {
+        votebars.push({
+          color: 'red',
+          x: (1.5 + (i * 1.2))
+        });
+      }
+    } else {
+      for (var i = 0; i < 10; i++) {
+        votebars.push({
+          color: 'grey',
+          x: (1.5 + (i * 1.2))
+        });
+      }
+    }
+
+    var currentKarma = (this.plusVotes - this.minVotes);
+    var karma = (currentKarma < 0) ? '$f00' : '$0f0+';
+    karma += currentKarma;
+
+    var widgetOptions = {
+      votebars: votebars,
+      positivevotes: this.plusVotes,
+      negativevotes: this.minVotes,
+      karmapercentage: parseFloat(((this.plusVotes/this.votes.length) * 100)).toFixed(1),
+      karma: karma,
+      positive: false,
+      negative: false
+    };
+
+    var playerVote = this.votes.filter(function (vote) {
+      return vote.PlayerId == player.id;
+    });
+
+    if(playerVote.length == 1) {
+      var vote = playerVote[0];
+
+      if(vote.score == 1)
+        widgetOptions.positive = true;
+      else if(vote.score == -1)
+        widgetOptions.negative = true;
+    }
+
+    this.karmaWidget.player(player.login, widgetOptions).update();
+  }
+
+  /**
    * Lets the player vote ++ on the current map.
    *
    * @param player
    */
-  votePositive(player) {
+  votePositive(player, widget) {
     var playerVote = this.votes.filter(function (vote) {
       return vote.PlayerId == player.id;
     });
 
     var voteResult = '$ff0';
     if(playerVote.length == 1 && playerVote[0].score == 1) {
-      voteResult += 'You already voted $fff++$ff0 for this map!';
+      if(widget) {
+        voteResult += 'Removed your $fff++$ff0 vote!';
+        this.votes.splice(this.votes.indexOf(playerVote[0]), 1);
+        playerVote[0].destroy();
+        this.plusVotes--;
+      } else {
+        voteResult += 'You already voted $fff++$ff0 on this map!';
+      }
     } else if(playerVote.length == 1 && playerVote[0].score == -1) {
       voteResult += 'Succesfully changed your vote from $fff--$ff0 to $fff++$ff0!';
       playerVote[0].score = 1;
@@ -175,7 +345,14 @@ module.exports.default = class extends Plugin {
     }
 
     this.server.send().chat(voteResult, {destination: player.login}).exec();
-    this.displayVotes(player);
+
+    if(!widget)
+      this.displayVotes(player);
+
+    Object.keys(this.players.list).forEach((login) => {
+      let player = this.players.list[login];
+      this.displayKarmaWidget(player);
+    });
   }
 
   /**
@@ -183,14 +360,21 @@ module.exports.default = class extends Plugin {
    *
    * @param player
    */
-  voteNegative(player) {
+  voteNegative(player, widget) {
     var playerVote = this.votes.filter(function (vote) {
       return vote.PlayerId == player.id;
     });
 
     var voteResult = '$ff0';
     if(playerVote.length == 1 && playerVote[0].score == -1) {
-      voteResult += 'You already voted $fff--$ff0 for this map!';
+      if(widget) {
+        voteResult += 'Removed your $fff--$ff0 vote!';
+        this.votes.splice(this.votes.indexOf(playerVote[0]), 1);
+        playerVote[0].destroy();
+        this.minVotes--;
+      } else {
+        voteResult += 'You already voted $fff--$ff0 on this map!';
+      }
     } else if(playerVote.length == 1 && playerVote[0].score == 1) {
       voteResult += 'Succesfully changed your vote from $fff++$ff0 to $fff--$ff0!';
       playerVote[0].score = -1;
@@ -215,6 +399,13 @@ module.exports.default = class extends Plugin {
     }
 
     this.server.send().chat(voteResult, {destination: player.login}).exec();
-    this.displayVotes(player);
+
+    if(!widget)
+      this.displayVotes(player);
+
+    Object.keys(this.players.list).forEach((login) => {
+      let player = this.players.list[login];
+      this.displayKarmaWidget(player);
+    });
   }
-}
+};
