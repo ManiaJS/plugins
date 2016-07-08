2682c0f456fdd4c90ba3f251dd8c6db0b7d77a77
Max Klaversma <maxklaversma@gmail.com>
2016-07-08 23:11:38 +0200

    Added setting to switch comparison time (needs foreignkey adjustment for SettingManager).

--- packages/maniajs-cpdifference/src/plugin.js
+++ packages/maniajs-cpdifference/src/plugin.js
@@ -24,6 +24,7 @@ module.exports.default = class extends Plugin {
     // Add dependencies, enter module full id's (mostly npm package names) here.
     // TODO: Should include '@maniajs/plugin-localrecords' as dependency, but this causes a fatal error.
     this.dependencies = [/*'@maniajs/plugin-localrecords'*/];
+    this.players = [];
   }
 
   /**
@@ -43,7 +44,92 @@ module.exports.default = class extends Plugin {
     this.server.on('trackmania.player.checkpoint',
       (params) => this.playerCheckpoint(params));
 
+    this.server.on('player.connect', (params) => {
+      let player = this.players.list[params.login];
+      this.playerRecord(player);
+    });
+
+    this.server.on('player.disconnect', (params) => {
+      delete this.players[params.login];
+    });
+
+    Object.keys(this.players.list).forEach((login) => {
+      let player = this.players.list[login];
+      this.loadPlayerSetting(player);
+    });
+
+    this.server.command.on('cpdifference', 'Change your CP Difference setting', (player, params) => this.setPlayerSettingCommand(player, params));
+    this.server.command.on('cps', 'Change your CP Difference setting', (player, params) => this.setPlayerSettingCommand(player, params));
+
     return Promise.resolve();
+  }
+
+  /**
+   * Loads CP Difference setting for player into the local playerlist.
+   *
+   * @param player
+   */
+  loadPlayerSetting(player) {
+    this.app.settings.getSettingWithForeignKey(this, 'cpDifference', player.id).then((setting) => {
+      if(!setting) {
+        this.setPlayerSetting(player, 'own');
+      } else {
+        this.players[player.login] = setting.value;
+      }
+    });
+  }
+
+  /**
+   * Saves CP Difference setting for player.
+   *
+   * @param player
+   * @param setting
+   */
+  setPlayerSetting(player, setting) {
+    this.players[player.login] = setting;
+    this.app.settings.setSettingWithForeignKey(this, 'cpDifference', setting, player.id);
+  }
+
+  /**
+   * Allows player to update their CP Difference setting.
+   * 
+   * @param player
+   * @param params
+   */
+  setPlayerSettingCommand(player, params) {
+    switch(params[0]) {
+      case 'own':
+        this.setPlayerSetting(player, 'own');
+        this.server.send().chat('$fffSet CP Difference to use own record (or last local in case of no record).', {destination: player.login}).exec();
+        break;
+      case 'local':
+        this.setPlayerSetting(player, 'local');
+        this.server.send().chat('$fffSet CP Difference to use first local record.', {destination: player.login}).exec();
+        break;
+      case 'dedi':
+        this.setPlayerSetting(player, 'dedi');
+        this.server.send().chat('$fffSet CP Difference to use first dedimania record.', {destination: player.login}).exec();
+        break;
+      case 'help':
+      default:
+        let ownBold = '';
+        let localBold = '';
+        let dediBold = '';
+
+        switch(this.players[player.login]) {
+          case 'own':
+            ownBold = '$o';
+            break;
+          case 'local':
+            localBold = '$o';
+            break;
+          case 'dedi':
+            dediBold = '$o';
+            break;
+        }
+        this.server.send().chat('$fffUsage: /cpdifference [$eee' + ownBold + 'own (or last)' + ownBold + ', ' + localBold + 'local' + localBold + ', ' + dediBold + 'dedi' + dediBold + '$fff]', {destination: player.login}).exec();
+        break;
+    }
   }
 
   /**
@@ -58,27 +144,50 @@ module.exports.default = class extends Plugin {
    */
   playerCheckpoint(checkpoint) {
     let player = this.players.list[checkpoint.login];
-    this.app.plugins['@maniajs/plugin-localrecords'].getPersonalMapRecord(this.maps.list[this.maps.current.uid], player).then((personalRecord) => {
-      let checkpoints = personalRecord.checkpoints.split(',');
-      let currentCheckpoint = checkpoint.timeOrScore;
-      let recordCheckpoint = checkpoints[checkpoint.checkpoint];
-      let difference = (currentCheckpoint - recordCheckpoint);
-      let improve = false;
+    if(this.players[player.login] == 'own') {
+      this.app.plugins['@maniajs/plugin-localrecords'].getPersonalMapRecord(this.maps.list[this.maps.current.uid], player).then((personalRecord) => {
+        let checkpoints = personalRecord.checkpoints.split(',');
+        let currentCheckpoint = checkpoint.timeOrScore;
+        let recordCheckpoint = checkpoints[checkpoint.checkpoint];
+        
+        this.displayUI(player, 'personal', currentCheckpoint, recordCheckpoint);
+      });
+    } else if(this.players[player.login] == 'local') {
+      this.app.plugins['@maniajs/plugin-localrecords'].getMapRecord(this.maps.list[this.maps.current.uid]).then((localRecord) => {
+        let checkpoints = localRecord.checkpoints.split(',');
+        let currentCheckpoint = checkpoint.timeOrScore;
+        let recordCheckpoint = checkpoints[checkpoint.checkpoint];
+        
+        this.displayUI(player, 'local', currentCheckpoint, recordCheckpoint);
+      });
+    }
+  }
 
-      let text = '';
-      if(difference < 0) {
-        improve = true;
-        difference *= -1;
+  /**
+   * Display UI to the player.
+   *
+   * @param player
+   * @param recordType
+   * @param current
+   * @param comparison
+   */
+  displayUI(player, recordType, current, comparison) {
+    let difference = (current - comparison);
+    let improve = false;
 
-        text += '$00C-';
-      } else {
-        text += '$C00+';
-      }
+    let text = '';
+    if(difference < 0) {
+      improve = true;
+      difference *= -1;
 
-      text += this.app.util.times.stringTime(difference);
+      text += '$00C-';
+    } else {
+      text += '$C00+';
+    }
 
-      this.cpWidget.timeout = 2000;
-      this.cpWidget.player(player.login, {difference: text}).update();
-    });
+    text += this.app.util.times.stringTime(difference);
+
+    this.cpWidget.timeout = 2000;
+    this.cpWidget.player(player.login, {difference: text, recordType: recordType}).update();
   }
 };
